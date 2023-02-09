ea20c66b4e75531e91e9154a412ed66e6367c8a4
Ram <121362638+ra-mss@users.noreply.github.com>
2023-02-09 12:22:26 -0600

    Create occurrence map (rnaturalearth and ggplot2)

--- occmap.r
+++ occmap.r
@@ -21,3 +21,16 @@ points(oysterc$decimalLongitude, oysterc$decimalLatitude, col="red", pch=2, cex=
 plot(wrld_simpl, xlim=c(-120,-85), ylim=c(14,33), axes=T)
 points(oysterc$decimalLongitude, oysterc$decimalLatitude, col="red", pch=2, cex=0.3)
 
+###Mapa de avistamientos 
+library(rnaturalearthdata)
+library(rnaturalearth)
+library(lubridate)
+world <- ne_countries(scale = 'medium', type = 'map_units', returnclass = 'sf')
+
+ggplot() +
+  geom_sf(data = world) + theme_dark() +
+  geom_point(data=oysterc, 
+             aes(x = decimalLongitude, y = decimalLatitude, col=year), pch=19, size=0.8) + 
+  ggtitle("World Map (rnaturalearth)")+
+  coord_sf(xlim = c(-30,-170), ylim = c(-57,74)) 
+
